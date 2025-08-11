be636a4c67fe8882aed83214d860d27d2bc67b6e
SABA TORONJADZE <soull@gmail.com>
2025-08-11 14:52:48 -0700

    CHAVAMATE RAGACEEBI

--- script.js
+++ script.js
@@ -1,93 +1,107 @@
 const rollButton = document.querySelector(".rollButton");
 const mainImage = document.querySelector(".mainImage");
 const currentPointOne = document.querySelector(".currentPointOne");
-const leftDiv = document.querySelector(".leftDiv")
-const rightDiv  = document.querySelector(".rightDiv")
-const holdButton = document.querySelector(".holdButton")
-const playerOnePoint = document.querySelector(".playerOnePoint")
+const currentPointTwo = document.querySelector(".currentPointTwo");
+const leftDiv = document.querySelector(".leftDiv");
+const rightDiv = document.querySelector(".rightDiv");
+const holdButton = document.querySelector(".holdButton");
+const playerOnePoint = document.querySelector(".playerOnePoint");
+const playerTwoPoint = document.querySelector(".playerTwoPoint");
 
-let x = 0;
-let player  = 1;
+let current = 0;
+let player = 1;
 
+rollButton.addEventListener("click", function () {
+  const random = Math.floor(Math.random() * 6) + 1;
 
-function ChangePlayer(){
-  if(player === 1){
-    player = 2;
-  }else if ( player === 2 ) {
-    player = 1;
+  if (random === 1) {
+    mainImage.src = "dice-1.png";
+    CurrentSum(0);
+    ClearCurrent();
+    ChangePlayer();
+  } else if (random === 2) {
+    mainImage.src = "dice-2.png";
+    CurrentSum(random);
+  } else if (random === 3) {
+    mainImage.src = "dice-3.png";
+    CurrentSum(random);
+  } else if (random === 4) {
+    mainImage.src = "dice-4.png";
+    CurrentSum(random);
+  } else if (random === 5) {
+    mainImage.src = "dice-5.png";
+    CurrentSum(random);
+  } else if (random === 6) {
+    mainImage.src = "dice-6.png";
+    CurrentSum(random);
   }
+});
 
-
-}
-
-
-function ChangeColor() {
-  if (player === 1){
-  leftDiv.style.backgroundColor = " rgba(255, 255, 255, 0.5)"
-  rightDiv.style.backgroundColor = " rgba(255, 255, 255, 0.2) "
-} else if ( player === 2){ 
-  rightDiv.style.backgroundColor = "  rgba(255, 255, 255, 0.5) "
-  leftDiv.style.backgroundColor = " rgba(255, 255, 255, 0.2)"
-}
-
-
-}
-
-
-
-
-
-rollButton.addEventListener(
+holdButton.addEventListener(
   "click",
 
   function () {
-    const random = Math.floor(Math.random() * 6) + 1;
-    
-    
-
-      
-
-
-    if (random === 1) {
-      mainImage.src = "./dice-1.png";
-      x = 0 ;
-      currentPointOne.textContent = x;
-      if(player === 1){
-        player = 2
-      }
-      ChangeColor();
-    } else if (random === 2) {
-      mainImage.src = "./dice-2.png";
-      x = x + 2 
-      currentPointOne.textContent = x;
-      if( player === 2) {
-        player = 1
-      }
-      ChangeColor();
-    } else if (random === 3) {
-      mainImage.src = "./dice-3.png";
-      x = x + 3; 
-      currentPointOne.textContent = x;
-    } else if (random === 4) {
-      mainImage.src = "./dice-4.png";
-      x = x + 4;
-      currentPointOne.textContent = x;
-    } else if (random === 5) {
-      mainImage.src = "./dice-5.png";
-      x = x + 5;
-      currentPointOne.textContent = x;
-    } else {
-      mainImage.src = "./dice-6.png";
-      x = x + 6; 
-      currentPointOne.textContent = x;
-    }
+    UpdatePlayer();
+    ChangePlayer();
+    Winner();
   }
 );
-holdButton.addEventListener (
-  `click`,
-  function() {
-    
-    playerOnePoint.textContent ="1"
-    ChangePlayer
+
+function ChangePlayer() {
+  if (player === 1) {
+    player = 2;
+  } else if (player === 2) {
+    player = 1;
   }
-);
+
+  if (player === 1) {
+    leftDiv.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
+    rightDiv.style.backgroundColor = "rgba(255, 255, 255, 0.2)";
+  } else if (player === 2) {
+    leftDiv.style.backgroundColor = "rgba(255, 255, 255, 0.2)";
+    rightDiv.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
+  }
+}
+
+function CurrentSum(value) {
+  if (player === 1) {
+    current = current + value;
+    currentPointOne.textContent = current;
+  } else if (player === 2) {
+    current = current + value;
+    currentPointTwo.textContent = current;
+  }
+}
+
+function UpdatePlayer() {
+  if (player === 1) {
+    playerOnePoint.textContent = Number(playerOnePoint.textContent) + current;
+    current = 0;
+    currentPointOne.textContent = current;
+  } else if (player === 2) {
+    playerTwoPoint.textContent = Number(playerTwoPoint.textContent) + current;
+    current = 0;
+    currentPointTwo.textContent = current;
+  }
+}
+
+
+function ClearCurrent() {
+  current = 0;
+  if(player === 1) {
+    currentPointOne.textContent = current;
+  } else {
+    currentPointTwo.textContent = current;
+  }
+
+  
+}
+function Winner() {
+  if (Number(playerOnePoint.textContent) >= 10) {
+    alert("Winner Player One !");
+  }
+
+  if (Number(playerTwoPoint.textContent) >= 10) {
+    alert("Winner Player Two !");
+  }
+}
